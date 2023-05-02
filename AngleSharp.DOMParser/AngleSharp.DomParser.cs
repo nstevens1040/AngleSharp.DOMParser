241e0e97fe319c5f7c59363aeb88ec33186bb8b7
Nicholas Stevens <nstevens@nanick.org>
2023-05-02 07:40:02 -0500

    Add project files.

---
+++ AngleSharp.DOMParser/AngleSharp.DomParser.cs
@@ -0,0 +1,24 @@
+﻿namespace AngleSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AngleSharp;
+    using AngleSharp.Dom;
+    //
+    public class DomParser
+    {
+        public static IDocument GetDomDocument(string HtmlString)
+        {
+            Task<IDocument> doc = Task.Factory.StartNew(async () =>
+            {
+                IBrowsingContext context = BrowsingContext.New(Configuration.Default);
+                IDocument d = await context.OpenAsync(req => req.Content(HtmlString));
+                return d;
+            }).Result;
+            return doc.Result;
+        }
+    }
+}
---
+++ AngleSharp.DOMParser/build.ps1
@@ -0,0 +1,43 @@
+
+    [cmdletbinding()]
+    Param()
+    cd ..\..\
+    #cd [System.IO.FileInfo]::New($MyInvocation.MyCommand.Path).Directory.FullName
+    remove-item "$($PWD.Path)\ilmerge.3.0.41.nupkg" -ea 0
+    remove-item -Recurse -Force -Path "$($PWD.Path)\ilmerge.3.0.41" -ea 0
+    remove-item "$($PWD.Path)\ilmerge.3.0.41.zip" -ea 0
+    if((Get-ExecutionPolicy) -notin @([Microsoft.PowerShell.ExecutionPolicy]::Unrestricted,[Microsoft.PowerShell.ExecutionPolicy]::Bypass))
+    {
+        if(
+            [Security.Principal.WindowsPrincipal]::New([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+        ){
+            Set-ExecutionPolicy -Scope Process Bypass -Force
+            Set-ExecutionPolicy -Scope CurrentUser Bypass -Force
+            Set-ExecutionPolicy -Scope LocalMachine Bypass -Force
+            Set-ExecutionPolicy Bypass -Force
+        } else {
+            Set-ExecutionPolicy -Scope Process Bypass -Force
+            Set-ExecutionPolicy -Scope CurrentUser Bypass -Force
+        }
+    }
+    [System.Net.WebClient]::New().DownloadFile(
+        "https://globalcdn.nuget.org/packages/ilmerge.3.0.41.nupkg",
+        "$($PWD.Path)\ilmerge.3.0.41.nupkg"
+    )
+    $null = mkdir "$($PWD.Path)\ilmerge.3.0.41"
+    Move-Item "$($PWD.Path)\ilmerge.3.0.41.nupkg" "$($PWD.Path)\ilmerge.3.0.41.zip"
+    Expand-Archive -Path "$($PWD.Path)\ilmerge.3.0.41.zip" -Destination "$($PWD.Path)\ilmerge.3.0.41"
+    $ilmerge = "$($PWD.Path)\ilmerge.3.0.41\tools\net452\ILMerge.exe"
+    if(!(Test-Path ".\build" -ea 0)){ $null = mkdir ".\build" }
+    . $ilmerge /lib:.\bin\Debug `
+    /log:.\AngleSharp.DomParser.ILMerge.log `
+    /target:library `
+    /targetplatform:v4 `
+    /out:.\build\AngleSharp.DomParser.dll `
+    .\bin\Debug\AngleSharp.DOMParser.dll `
+    .\bin\Debug\System.Text.Encoding.CodePages.dll `
+    .\bin\Debug\AngleSharp.dll `
+    .\bin\Debug\System.Buffers.dll `
+    .\bin\Debug\System.Memory.dll `
+    .\bin\Debug\System.Numerics.Vectors.dll `
+    .\bin\Debug\System.Runtime.CompilerServices.Unsafe.dll        
